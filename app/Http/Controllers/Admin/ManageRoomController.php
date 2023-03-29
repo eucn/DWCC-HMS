c3fd943649c4070199a05c2a2aef14ba37434a21
CamilleJane <camillejanemendoza29@gmail.com>
2023-03-29 13:25:26 +0800

    show image

--- app/Http/Controllers/Admin/ManageRoomController.php
+++ app/Http/Controllers/Admin/ManageRoomController.php
@@ -28,15 +28,34 @@ class ManageRoomController extends Controller
             'amenities' => 'required',
             'status' => 'required',
             'rate' => 'required|numeric',
-            'photos.*' => 'nullable|image|mimes:jpeg,png,jpg,gif',
+            'photos.*' => 'nullable|image|mimes:jpeg,png,jpg,gif,max:2048',
         ]);
 
-        $requestData = $request->all();
-        $fileName = time().$request->file('photos')->getClientOriginalName();
-        $path = $request->file('photos')->storeAs('images', $fileName, 'public');
-        $requestData["photos"] = '/storage/'.$path;
-        Manage_Room::create($requestData);
-        return redirect()->route('admin.room.index')->with('success', 'The Room has been successfully added.');
+        $rooms = new Manage_Room;
+
+        $rooms->room_number = $request->input('room_number');
+        $rooms->room_type = $request->input('room_type');
+        $rooms->room_description = $request->input('room_description');
+        $rooms->max_capacity = $request->input('max_capacity');
+        $rooms->amenities = $request->input('amenities');
+        $rooms->status = $request->input('status');
+        $rooms->rate = $request->input('rate');
+        // $rooms->photos = $request->input('photos');
+
+        if($request->hasfile('photos')){
+            $file = $request->file('photos');
+            $extension = $file->getClientOriginalExtension(); //getting image extension
+            $filename = time() . '.' . $extension;
+            $file->move('uploads/rooms/', $filename);
+            $rooms["photos"] = $filename;
+        }else{
+            return $request;
+            $rooms["photos"] = "";
+        }
+
+        $rooms->save();
+
+        return redirect()->route('admin.room.index')->with('success', 'The room has been successfully added.');
     }
 
     public function edit($id){
@@ -56,34 +75,19 @@ class ManageRoomController extends Controller
          return redirect()->route('admin.room.index')->with('success', 'Room has been deleted.')->with('rooms', $room);;
      }
 
-//      public function update(Request $request, $id)
-// {
-//     $room = manage_room::findOrFail($id);
-
-//     if ($room) {
-//         $room->room_number = $request->input('room_number');
-//         $room->room_description = $request->input('room_description');
-//         $room->room_type = $request->input('room_type');
-//         $room->max_capacity = $request->input('max_capacity');
-//         $room->amenities = $request->input('amenities');
-//         $room->status = $request->input('status');
-//         $room->rate = $request->input('rate');
-//         $room->update($request->all());
-//         $room->save();
-        
-//         // return redirect()->route('admin.room.index')->with('success', 'Room has been updated.')->with('room', $room);
-//         return redirect()->back()->with('success', 'Room has been updated.')->with('room', $room);
-//     }
-//     else{
-//     return redirect()->route('admin.room.index')->with('not success', 'Room did not updated.')->with('room', $room);}
-
-    
-
-
-// }
-
 public function update(Request $request, $id)
     {
+        $validatedData = $request->validate([
+            'room_number' => 'required|numeric',
+            'room_type' => 'required',
+            'room_description' => 'required',
+            'max_capacity' => 'required|numeric',
+            'amenities' => 'required',
+            'status' => 'required',
+            'rate' => 'required|numeric',
+            'photos.*' => 'nullable|image|mimes:jpeg,png,jpg,gif,max:2048',
+        ]);
+
         $room = Manage_Room::find($id);
 
         if (!$room) {
@@ -96,11 +100,23 @@ public function update(Request $request, $id)
                 $room->status = $request->input('status');
                 $room->rate = $request->input('rate');
                 $room->update($request->all());
+
+                if($request->hasfile('photos')){
+                    $file = $request->file('photos');
+                    $extension = $file->getClientOriginalExtension(); //getting image extension
+                    $filename = time() . '.' . $extension;
+                    $file->move('uploads/rooms/', $filename);
+                    $rooms["photos"] = $filename;
+                }else{
+                    return $request;
+                    $rooms["photos"] = "";
+                }
+
                 $room->save();
 
         $room->save();
 
-        return redirect()->route('admin.room.index')->with('success', 'Room is updated.')->with('rooms', $room);
+        return redirect()->route('admin.room.index')->with('success', 'The room has been successfully added.')->with('rooms', $room);
     }
 
 

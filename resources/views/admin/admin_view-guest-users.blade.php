db8025c91c22e32110474e0a94359df5bfe008a9
jojieshar <pasionjojiesharine@gmail.com>
2023-03-31 15:12:05 +0800

    admin modal

--- resources/views/admin/admin_view-guest-users.blade.php
+++ resources/views/admin/admin_view-guest-users.blade.php
@@ -235,16 +235,16 @@
               <!--View Button-->	
               <!-- <center>
                   <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal_viewsuserdetails" data-profile="../profile-pictures/default-profile.png" data-fname="Nikolas" data-mname="G." data-lname="Ocampo" data-gender="" data-email="" data-batch="2023" data-program="MIT" data-nprog="1"><svg class="svg-inline--fa fa-eye" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="eye" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M279.6 160.4C282.4 160.1 285.2 160 288 160C341 160 384 202.1 384 256C384 309 341 352 288 352C234.1 352 192 309 192 256C192 253.2 192.1 250.4 192.4 247.6C201.7 252.1 212.5 256 224 256C259.3 256 288 227.3 288 192C288 180.5 284.1 169.7 279.6 160.4zM480.6 112.6C527.4 156 558.7 207.1 573.5 243.7C576.8 251.6 576.8 260.4 573.5 268.3C558.7 304 527.4 355.1 480.6 399.4C433.5 443.2 368.8 480 288 480C207.2 480 142.5 443.2 95.42 399.4C48.62 355.1 17.34 304 2.461 268.3C-.8205 260.4-.8205 251.6 2.461 243.7C17.34 207.1 48.62 156 95.42 112.6C142.5 68.84 207.2 32 288 32C368.8 32 433.5 68.84 480.6 112.6V112.6zM288 112C208.5 112 144 176.5 144 256C144 335.5 208.5 400 288 400C367.5 400 432 335.5 432 256C432 176.5 367.5 112 288 112z"></path></svg><!-- <i class="fa-solid fa-eye"></i> Font Awesome fontawesome.com -->
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable{{ $user->id }}">
-                    <i class="fa-solid fa-eye">View</i>
+                <button type="button" style = "width: 35px; height: 30px; position: relative; left: 40px;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDialogScrollable{{ $user->id }}">
+                <i class="fa-regular fa-eye" style="position: relative;left: -4px; top: -4px;"></i>
                     </button>
 
                      <!-- Button trigger activate modal -->
- <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#activate{{ $user->id }}">
-    <i class="fa-solid fa-user"></i>
+ <button type="button" style="position: relative; left: 40px;" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#activate{{ $user->id }}">
+ <i class="fa-sharp fa-solid fa-user-check"></i>
 </button>
                         <!-- Button trigger deactivate modal -->
-                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deactivate{{ $user->id }}">
+                        <button type="button" style="position: relative; left: 40px;" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deactivate{{ $user->id }}">
     <i class="fa-solid fa-user-slash"></i>
 </button>
 
@@ -256,8 +256,9 @@
                       <div class="modal-dialog modal-dialog-scrollable">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title">View Information</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 class="modal-title" style="color: #55afab;">View Information</h5>
+                            <button type="button" style="position:relative; left: 1px; color: #dc3545;" class="close"
+                        data-dismiss="modal">&times;</button>
                           </div>
                           <div class="modal-body">
                           <h6><strong>Name:</strong> {{ $user->name }}</h6><br>
@@ -280,8 +281,9 @@
                 @csrf
                 @method('PATCH')
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Deactivate User</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title" id="exampleModalLabel"  style="color: #55afab;">Deactivate User</h5>
+                    <button type="button" style="position:relative; left: 1px; color: #dc3545;" class="close"
+                        data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
                     <p>Are you sure you want to deactivate this user?</p>
@@ -303,15 +305,16 @@
                 @csrf
                 @method('PATCH')
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Activate User</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title" id="exampleModalLabel " style="color: #55afab;">Activate User</h5>
+                    <button type="button" style="position:relative; left: 1px; color: #dc3545;" class="close"
+                        data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
                     <p>Are you sure you want to activate this user?</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-success">Activate</button>
+                    <button type="submit" class="btn btn-primary">Activate</button>
                 </div>
             </form>
         </div>

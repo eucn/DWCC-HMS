052c78431e8f74b3dd6303bebc93e92e257de7a7
eucn <eubertnovencido20@gmail.com>
2023-04-12 13:45:08 +0800

    add column Acc_Stat for Activate and Deactivate

--- database/migrations/2023_02_26_180836_create_frontdesks_table.php
+++ database/migrations/2023_02_26_180836_create_frontdesks_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('Acc_Stat')->default('Activate');
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
